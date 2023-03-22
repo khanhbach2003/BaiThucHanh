3c51d36866d17c4153e5f35bb852d1c418d71d53
unknown <you@example.com>
2023-03-22 15:29:31 +0700

    BaiThucHanhLab2

--- BaiThucHanhLab3/src/App.java
+++
@@ -1,8 +0,0 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        Hinhtron ht = new Hinhtron();
-        ht.nhapbankinh();
-        ht.tinhdientich();
-        ht.indientich();
-    }
-}
