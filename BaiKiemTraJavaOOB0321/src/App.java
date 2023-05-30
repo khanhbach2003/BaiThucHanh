c0bac42db76bf5d9869b69dddbe57d6484aee32b
unknown <you@example.com>
2023-05-30 08:32:52 +0700

    TKB0321STUDENT

--- BaiKiemTraJavaOOB0321/src/App.java
+++ BaiKiemTraJavaOOB0321/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      Scanner sc = new Scanner(System.in);
+    
     }
 }
